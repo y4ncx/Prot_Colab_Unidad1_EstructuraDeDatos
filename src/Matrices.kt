56348c8b86ecd0f322f6494f2760228ec435823a
y4ncx <y4ncxs@gmail.com>
2025-09-14 14:15:15 -0500

    Se agrega el segundo ejercicio de matrices resuelto

--- src/Matrices.kt
+++ src/Matrices.kt
@@ -1,7 +1,27 @@
+fun main(){
+    matrices()
+}
+
 fun matrices(){
 
     val matriz = Array(3) { fila ->
         Array(3) { col -> fila * 3 + col + 1 }
     }
+
+    // Imprimir en forma de tabla
+    println("\nMatriz en forma de tabla:")
+    for (fila in matriz) {
+        println(fila.joinToString("\t"))
+    }
+
+    // Recorrer por columnas
+    println("\nRecorrido por columnas:")
+    for (c in 0 until 3) {
+        for (f in 0 until 3) {
+            print("${matriz[f][c]} ")
+        }
+        println()
+    }
+
 
 }
