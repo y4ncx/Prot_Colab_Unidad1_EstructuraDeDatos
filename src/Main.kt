be2856ce3fefcc100e7857c1493221132ff8c267
y4ncx <y4ncxs@gmail.com>
2025-09-14 13:41:18 -0500

    Se agrega el segundo ejercicio resuelto

--- src/Main.kt
+++ src/Main.kt
@@ -4,4 +4,19 @@ fun main() {
 
     println("Arreglo inicial:")
     println(arreglo.joinToString(", "))
+
+
+    // Recorrido con for clásico
+    println("\nRecorrido con for clásico:")
+    for (i in arreglo.indices) {
+        print("${arreglo[i]} ")
+    }
+
+    // Recorrido con for-each
+    println("\n\nRecorrido con for-each:")
+    for (valor in arreglo) {
+        print("$valor ")
+    }
+    
+
 }
